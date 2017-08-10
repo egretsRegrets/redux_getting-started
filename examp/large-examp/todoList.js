ff3ebf7e554f60d891df7d63dd7c11da4f2db863
mercantilism <samuel.galen.gardner@gmail.com>
2017-08-10 16:17:15 -0700

    todo list - with add and toggle methods - and visibilty filter for todos, based on toggle status - react and redux

--- examp/large-examp/todoList.js
+++ examp/large-examp/todoList.js
@@ -127,6 +127,140 @@ const todoApp = combineReducers({
 // initialize store:
 const store = redux.createStore(todoApp);
 
+// consuming state for views
+
+const FilterLink = ({
+    filter,
+    currentFilter,
+    children
+}) => {
+    if (filter === currentFilter) {
+        return <span>{children}</span>;
+    }
+    
+    return (
+        <a href='#'
+            onClick={e => {
+                e.preventDefault();
+                store.dispatch({
+                    type: 'SET_VISIBILITY_FILTER',
+                    filter
+                });
+            }}
+        >
+            {children}
+        </a>
+    );
+};
+
+const getVisibleTodos = ( todos, filter) => {
+    switch (filter){
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_ACTIVE':
+            return todos.filter(
+                t => !t.completed
+            );
+        case 'SHOW_COMPLETED':
+            return todos.filter(
+                t => t.completed
+            );
+        default:
+            return todos;
+    }
+};
+
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+    render() {
+        /**
+         * use es6 object literal shorthand
+         * to destructure props into its component properties
+         */
+        const {
+            todos,
+            visibilityFilter
+        } = this.props;
+        const visibleTodos = getVisibleTodos(
+            todos,
+            visibilityFilter
+        );
+        return (
+            <div>
+                <input ref={node => {
+                    this.input = node;
+                }} />
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: this.input.value,
+                        id: nextTodoId++
+                    });
+                    this.input.value = '';
+                }}>
+                    Add Todo
+                </button>
+                <ul>
+                    {this.visibleTodos.map(todo =>
+                        <li key={todo.id}
+                            onClick={() => {
+                                store.dispatch({
+                                    type: 'TOGGLE_TODO',
+                                    id: todo.id
+                                });
+                            }}
+                            style={{
+                                textDecoration:
+                                    todo.completed ?
+                                        'line-through' : 
+                                        'none'
+                            }}>
+                            {todo.text}
+                        </li>
+                    )}
+                </ul>
+                <p>
+                    Show:
+                    {' '}
+                    <FilterLink
+                        filter='SHOW_ALL'
+                        currentFilter={visibilityFilter}
+                    >
+                        All
+                    </FilterLink>
+                    {' '}
+                    <FilterLink
+                        filter='SHOW_ACTIVE'
+                        currentFilter={visibilityFilter}
+                    >
+                        Active
+                    </FilterLink>
+                    {' '}
+                    <FilterLink
+                        filter='SHOW_COMPLETED'
+                        currentFilter={visibilityFilter}
+                    >
+                        Completed
+                    </FilterLink>
+                </p>    
+            </div>
+        );
+    }
+}
+
+const render = () => {
+    ReactDom.render(
+        <TodoApp
+            // will give us all properties of store
+            {...store.getState()}
+        />,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(render);
+render(); // render initial state
+
 //tests:
 
 const testAddTodo = () => {
